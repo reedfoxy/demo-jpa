57fc6433bf0573515857c0ffacdcc9c1483d9376
kbs <kbs@hoyun.co.kr>
2020-05-18 02:09:24 +0900

    feat : QueryDSL 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,8 +1,15 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 import javax.persistence.EntityManager;
 import java.util.List;
 
@@ -23,17 +30,50 @@ public class OrderRepository {
     }
 
     // 검색 조회
-    public List<Order> findAll(OrderSearch orderSearch) {
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        if(orderSearch.getMemberName() == null) {
+//            return em.createQuery("select o from  Order o", Order.class).setMaxResults(1000).getResultList();
+//        }
+//
+//        return em.createQuery("select o from  Order o join o.member m where o.status = :status and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000) // 최대 1000건
+//                .getResultList();
+//    }
 
-        if(orderSearch.getMemberName() == null) {
-            return em.createQuery("select o from  Order o", Order.class).setMaxResults(1000).getResultList();
+    public List<Order> findAll(OrderSearch orderSearch){
+
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+
+        if (statusCond == null) {
+            return null;
         }
+        return QOrder.order.status.eq(statusCond);
+    }
 
-        return em.createQuery("select o from  Order o join o.member m where o.status = :status and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) // 최대 1000건
-                .getResultList();
+    private BooleanExpression nameLike(String nameCond) {
+
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return QMember.member.name.like(nameCond);
     }
 
     public List<Order> findAllWithMemberDelivery() {
